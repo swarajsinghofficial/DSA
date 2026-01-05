14970a0c820ea09782603e03077e3cacf101eb02
Swaraj Singh <swarajsinghofficial@gmail.com>
2026-01-06 02:51:18 +0530

    Pyramid pattern

---
+++ pattern03.java
@@ -0,0 +1,14 @@
+public class pattern03 {
+    public static void pattern(){
+        for(int i =0; i<5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }    
+    public static void main(String[] args) {
+        pattern();
+    }
+}

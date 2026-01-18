7ee0295117c4a0cc75cadf7b3b43bd57546cab32
Swaraj Singh <swarajsinghofficial@gmail.com>
2026-01-18 23:46:51 +0530

    Learning Recursion

--- Recursion/RecursionBt2.java
+++ Recursion/RecursionBt2.java
@@ -1,12 +1,16 @@
+import java.util.Scanner;
 public class RecursionBt2 {
-    public static void RBt2(int n){
-        if (n<1){
+    public static void RBt2(int i, int n){
+        if (i<1){
             return;
         }
-        System.out.println(n);
-        RBt2(n-1);
+        RBt2(i-1,n);
+        System.out.println(i);
     }
     public static void main(String[] args) {
-        RBt2(5);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number upto which you want to print:");
+        int n = sc.nextInt();
+        RBt2(n,n);
     }
 }

b614b87e5003391a77aedb56fa327050cf7fcaca
Swaraj Singh <swarajsinghofficial@gmail.com>
2026-01-29 13:38:49 +0530

    2D Array

---
+++ Arrays/Array2d.java
@@ -0,0 +1,16 @@
+public class Array2d {
+    public static void main(String[] args){
+        int[][] arr = {{10,20},{30,40,50}};
+
+        for(int i =0; i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                System.out.println(arr[i][j]);
+            }
+        }
+
+
+        
+
+    }
+    
+}
